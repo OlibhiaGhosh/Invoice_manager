b562dcfd19c1d172173333734659f567cc180aaf
Olibhia Ghosh <olibhiag@gmail.com>
2026-02-05 13:41:22 +0530

    Fixed auth

--- app/api/invoice-details/[id]/route.ts
+++ app/api/invoice-details/[id]/route.ts
@@ -2,8 +2,7 @@ import { db } from "@/db";
 import { NextRequest, NextResponse } from "next/server";
 import { clients, invoices } from "@/db/schema";
 import { eq } from "drizzle-orm";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { auth } from "@/auth";
 
 export async function GET(req: NextRequest, context : { params: { id: string } }) {
     const { id } = await context.params;   // ✅ unwrap promise
@@ -16,7 +15,7 @@ export async function GET(req: NextRequest, context : { params: { id: string } }
   }
 
   const invoice_id = Number(id);
-    const session = await getServerSession(authOptions);
+    const session = await auth();
     const user_id = session?.user?.id;
 
     try {
